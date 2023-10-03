8dc75b886157cd77cffd13bd83a08db1f6c80bb6
loana14 <loanapumayali14@gmail.com>
2023-10-02 19:18:59 -0500

    Primer commit

---
+++ src/main/java/org/ciberfarma/controller/ProductoController.java
@@ -0,0 +1,36 @@
+package org.ciberfarma.controller;
+
+import org.ciberfarma.model.Producto;
+import org.ciberfarma.repository.IProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProductoController {
+	
+	@Autowired /*Inyeccicon de las dependencia, que se usara en el controlador */
+	private IProductoRepository repo;
+	
+	@GetMapping ("/listar")
+	public String listadoProductos(Model model) {
+		model.addAttribute("lstProductos", repo.findAll());
+		return("listado");
+	}
+	
+	@GetMapping ("/cargar")
+	public  String cargarPag(Model model) {
+	model.addAttribute("producto", new Producto());
+	return "crudproductos";
+	}
+	
+	@PostMapping ("/grabar")
+	public  String grabarPag(@ModelAttribute Producto producto) {
+	System.out.println(producto);
+	repo.save(producto);
+	return "exito";
+	}
+}
